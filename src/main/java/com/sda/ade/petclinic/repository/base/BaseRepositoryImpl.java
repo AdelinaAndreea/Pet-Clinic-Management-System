75acf87dcca6e977bb7bbbae5886234450b0de49
Adelina Andreea Muntean <adelina.muntean2@yahoo.com>
2022-05-12 20:54:21 +0300

    Create PCMS-13  Create base repository classes.

---
+++ src/main/java/com/sda/ade/petclinic/repository/base/BaseRepositoryImpl.java
@@ -0,0 +1,131 @@
+package com.sda.ade.petclinic.repository.base;
+
+
+import com.sda.ade.petclinic.utils.SessionManager;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+import java.util.Optional;
+
+public class BaseRepositoryImpl<T, ID> implements BaseRepository<T, ID> {
+    private final Class<T> clazz;
+
+    public BaseRepositoryImpl(Class<T> clazz) {
+        this.clazz = clazz;
+    }
+
+    @Override
+    public Optional<T> findById(ID employeeNumber) {
+        try {
+            Session session = SessionManager.getSessionFactory().openSession();
+
+            T employee = session.find(clazz, employeeNumber);
+
+            session.close();
+            return Optional.ofNullable(employee);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public void create(T entity) {
+        Transaction transaction = null;
+        try {
+            Session session = SessionManager.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+
+            session.save(entity);
+
+            transaction.commit();
+            session.close();
+
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void update(T entity) {
+        Transaction transaction = null;
+        try {
+            Session session = SessionManager.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+
+            session.update(entity);
+
+            transaction.commit();
+            session.close();
+
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void delete(T entity) {
+        Transaction transaction = null;
+        try {
+            Session session = SessionManager.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+
+            session.delete(entity);
+
+            transaction.commit();
+            session.close();
+
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void deleteById(ID id) {
+        Transaction transaction = null;
+        try {
+            Session session = SessionManager.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+
+            T entity = session.find(clazz, id);
+            if (entity != null) {
+                session.delete(entity);
+            }
+
+            transaction.commit();
+            session.close();
+
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<T> findAll() {
+        try {
+            Session session = SessionManager.getSessionFactory().openSession();
+
+            List<T> entities = session.createQuery("from " + clazz.getName(), clazz).list();
+
+            session.close();
+            return entities;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return List.of();
+        }
+
+    }
+}
---
+++ src/main/java/com/sda/ade/petclinic/service/package-info.java
@@ -0,0 +1,2 @@
+package com.sda.ade.petclinic.service;
+//business logic
